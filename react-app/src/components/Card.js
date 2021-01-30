3b3eab2f2fc84c84aef106e37ae0a2b1d50e5729
Antobio17 <antoniojr997@gmail.com>
2021-01-30 21:31:02 +0100

    Update

--- react-app/src/components/Card.js
+++ react-app/src/components/Card.js
@@ -8,18 +8,8 @@ class Card extends Component {
     // The method responsible for displaying the view of the component
     render(){
         return (
-            <div className="flip-container">
-                <div className="Card">
-                    <div className="Front">
-                        <p>{this.props.title}</p>
-                    </div>
-                    <div className="Back">
-                        <p>Usuario habitual de HTML</p>
-                        <p>Usuario habitual de CSS</p>
-                        <p>Usuario habitual de JAVASCRIPT</p>
-                        <p>Usuario habitual de PYTHON</p>
-                    </div>
-                </div>
+            <div className="Card">
+                <p>{this.props.title}</p>
             </div>
         );
     }
@@ -27,19 +17,3 @@ class Card extends Component {
 }
 
 export default Card;
-
-// return (
-//     <div className="flip-container">
-//         <div className="Card">
-//             <div className="Front">
-//                 <p>{this.props.title}</p>
-//             </div>
-//             <div className="Back">
-//                 <p>Usuario habitual de HTML</p>
-//                 <p>Usuario habitual de CSS</p>
-//                 <p>Usuario habitual de JAVASCRIPT</p>
-//                 <p>Usuario habitual de PYTHON</p>
-//             </div>
-//         </div>
-//     </div>
-// );
