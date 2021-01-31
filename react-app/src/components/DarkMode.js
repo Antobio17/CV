e2dfc6eb30ba0a872549f57a1eb0a0e07869403e
Antobio17 <antoniojr997@gmail.com>
2021-01-31 12:37:54 +0100

    Dark Mode implemented

--- react-app/src/components/DarkMode.js
+++ react-app/src/components/DarkMode.js
@@ -22,5 +22,9 @@ class DarkMode extends Component {
 
 function switchTheme(e) {
     document.body.classList.toggle("Dark-Mode-Activated");
+    document.querySelectorAll('.Card').forEach(card => {
+        card.classList.toggle("Dark-Mode-Card-Activated");
+    });
 }
+
 export default DarkMode;
